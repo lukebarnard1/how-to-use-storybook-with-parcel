16cd96eae7ddae18511bc7e99881c4224743d23b
lukebarnard <luke.barnard99@gmail.com>
2023-01-18 13:55:53 +0100

    Use storybook component in the Parcel app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react'
+import { Button } from './stories/Button'
+
 export function App() {
   const [count, setCount] = useState(0)
-  return <button onClick={() => setCount(count + 1)}>
-    This button has been clicked {count} times
-  </button>;
+  return (
+    <Button
+      onClick={() => setCount(count + 1)}
+      label={`This button has been clicked ${count} times`}
+      primary
+    />
+  )
 }
